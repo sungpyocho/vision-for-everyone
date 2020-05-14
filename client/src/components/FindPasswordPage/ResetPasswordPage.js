52f16e66eacbeb2c89be81121350f2c03d8984af
sungpyocho <korra0501@gmail.com>
2020-05-14 23:51:19 +0900

    Add: Password Reset

--- client/src/components/FindPasswordPage/ResetPasswordPage.js
+++ client/src/components/FindPasswordPage/ResetPasswordPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Avatar from "@material-ui/core/Avatar";
 import Alert from "@material-ui/lab/Alert";
 import Button from "@material-ui/core/Button";
@@ -48,33 +48,9 @@ export default function ResetPasswordPage(props) {
 
   const [Password, setPassword] = useState(""); // 비번
   const [ConfirmPassword, setConfirmPassword] = useState(""); // 비번 확인
-  const [arePasswordsSame, setArePasswordsSame] = useState(false); // 두 비번이 같은가?
+  const [arePasswordsSame, setArePasswordsSame] = useState(true); // 두 비번이 같은가?
   const [isLinkValid, setIsLinkValid] = useState(true); // 링크가 유효한가?
-  const [isUsingPastPassword, setIsUsingPastPassword] = useState(null); // 과거 비밀번호를 또 입력했나?
-
-  const waitForValidation = async () => {
-    axios
-      .get("/api/users/reset", {
-        params: {
-          resetPwdToken: props.location.pathname.slice(7), // URL의 토큰부분만 잘라낸다
-        },
-      })
-      .then((res) => {
-        console.log("안", props.location.pathname.slice(7));
-        if ((res.data.message = "비밀번호 재설정 링크가 유효하지 않습니다")) {
-          console.log("어떡한다냐 지났어야");
-          setIsLinkValid(false);
-        } else {
-          console.log("유효혀");
-          setIsLinkValid(true);
-        }
-      })
-      .catch((err) => console.log(err));
-  };
-
-  useEffect(() => {
-    waitForValidation();
-  }, []);
+  const [isUsingPastPassword, setIsUsingPastPassword] = useState(false); // 과거 비밀번호를 또 입력했나?
 
   const passwordHandler = (event) => {
     setPassword(event.currentTarget.value);
@@ -93,17 +69,35 @@ export default function ResetPasswordPage(props) {
     } else if (Password !== ConfirmPassword) {
       // 2. 입력한 비밀번호가 서로 다르다면...? => Material UI의 화면상 Alert. Submit 버튼 밑에 뜨게.
       setArePasswordsSame(false);
+      setIsUsingPastPassword(false);
     } else {
+      setArePasswordsSame(true);
+      setIsUsingPastPassword(false);
+
       // axios로 백엔드 API와 통신
       axios
         .post("/api/users/reset", {
-          newPassword: Password,
+          password: Password,
+          resetPwdToken: props.location.pathname.slice(7),
         })
         .then((res) => {
           // 3. 비밀번호 재설정 링크가 유효하지 않다면..
+          if (res.data.message === "link not valid") {
+            setIsLinkValid(false);
+            setIsUsingPastPassword(false);
+          } else {
+            setIsLinkValid(true);
+            setIsUsingPastPassword(false);
+          }
           // 4. 예전에 사용한 비밀번호를 또 입력했다면..
+          if (res.data.pwdNotChanged === true) {
+            setIsUsingPastPassword(true);
+          }
           // 5. 전부 관문을 통과한다면 로그인 페이지로 리다이렉트
-          console.log(res);
+          if (res.data.success === true && res.data.userInfo) {
+            alert("비밀번호가 성공적으로 변경되었습니다.");
+            props.history.push("/login");
+          }
         })
         .catch((err) => {
           console.log(err);
@@ -123,8 +117,8 @@ export default function ResetPasswordPage(props) {
         </Typography>
         {!isLinkValid && (
           <Alert severity="error">
-            비밀번호 재설정 URL의 유효기간이 지났습니다. 비밀번호 찾기를 다시
-            진행해주세요.
+            비밀번호 재설정 URL의 유효기간이 지났습니다. <br />
+            비밀번호 찾기를 다시 진행해주세요.
           </Alert>
         )}
         {isLinkValid && (
@@ -158,6 +152,21 @@ export default function ResetPasswordPage(props) {
                   onChange={confirmPasswordHandler}
                 />
               </Grid>
+              {!arePasswordsSame && (
+                <Grid item xs={12}>
+                  <Alert severity="error">
+                    비밀번호를 동일하게 입력해주세요.
+                  </Alert>
+                </Grid>
+              )}
+              {isUsingPastPassword && (
+                <Grid item xs={12}>
+                  <Alert severity="error">
+                    저번에 동일한 비밀번호를 사용하셨네요. <br />
+                    다른 비밀번호를 설정해주세요.
+                  </Alert>
+                </Grid>
+              )}
             </Grid>
             <Button
               type="submit"
