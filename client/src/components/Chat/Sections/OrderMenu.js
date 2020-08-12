0e76d656b56b80638098940e93fa8325ce8eea09
sungpyocho <korra0501@gmail.com>
2020-08-12 23:58:47 +0900

    Add Menu Co-authored-by: Changhoon Park <mindflo@korea.ac.kr>

--- client/src/components/Chat/Sections/OrderMenu.js
+++ client/src/components/Chat/Sections/OrderMenu.js
@@ -12,6 +12,25 @@ import MuiDialogTitle from "@material-ui/core/DialogTitle";
 import IconButton from "@material-ui/core/IconButton";
 import CloseIcon from "@material-ui/icons/Close";
 import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+// 여기서부터는 메뉴, 이벤트 팝업을 이쁘게 띄워주기 위해 불러온 컴포넌트
+// 1. 메뉴 팝업용
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandLess from "@material-ui/icons/ExpandLess"; // 세부메뉴 열기
+import ExpandMore from "@material-ui/icons/ExpandMore"; // 세부메뉴 닫기
+import Divider from "@material-ui/core/Divider"; // 리스트 사이에 넣는 구분선
+
+// 2. 이벤트 팝업용
+// import Card from "@material-ui/core/Card";
+// import CardActionArea from "@material-ui/core/CardActionArea";
+// import CardActions from "@material-ui/core/CardActions";
+// import CardContent from "@material-ui/core/CardContent";
+// import CardMedia from "@material-ui/core/CardMedia";
+// import CardBackground from "../../../assets/kiwe-event.jpg";
 
 const styles = (theme) => ({
   root: {
@@ -46,27 +65,52 @@ const DialogTitle = withStyles(styles)((props) => {
 
 function OrderMenu() {
   const [openMenu, setOpenMenu] = React.useState(false);
-  const [openEvent, setOpenEvent] = React.useState(false);
+  const [openSubMenu, setOpenSubMenu] = React.useState(false);
+  const [openSubMenu2, setOpenSubMenu2] = React.useState(false);
+  const [openMap, setopenMap] = React.useState(false);
   const [openCall, setOpenCall] = React.useState(false);
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      minWidth: "50vw",
+      maxHeight: "80vh",
+    },
+    nested: {
+      paddingLeft: theme.spacing(4),
+    },
+    media: {
+      height: "200px",
+    },
+  }));
+
+  const classes = useStyles();
 
   const handleOpenMenu = () => {
     setOpenMenu(true);
-  };
-
-  const handleOpenMap = () => {
-    setOpenEvent(true);
-  };
-
-  const handleOpenCall = () => {
-    setOpenCall(true);
   };
 
   const handleCloseMenu = () => {
     setOpenMenu(false);
   };
 
+  const handleOpenSubMenu = () => {
+    setOpenSubMenu(!openSubMenu);
+  };
+
+  const handleOpenSubMenu2 = () => {
+    setOpenSubMenu2(!openSubMenu2);
+  };
+
+  const handleOpenMap = () => {
+    setopenMap(true);
+  };
+
   const handleCloseMap = () => {
-    setOpenEvent(false);
+    setopenMap(false);
+  };
+
+  const handleOpenCall = () => {
+    setOpenCall(true);
   };
 
   const handleCloseCall = () => {
@@ -75,8 +119,8 @@ function OrderMenu() {
 
   return (
     <MenuComponent>
-      {/* 메뉴 버튼 */}
-      <CustomButton onClick={handleOpenMenu}>메뉴</CustomButton>
+      {/* 메뉴판 버튼 */}
+      <CustomButton onClick={handleOpenMenu}>메뉴판</CustomButton>
       <Dialog
         open={openMenu}
         onClose={handleCloseMenu}
@@ -86,9 +130,74 @@ function OrderMenu() {
       >
         <DialogTitle id="menu-title">{"메뉴"}</DialogTitle>
         <DialogContent>
-          <DialogContentText id="menu-description">
-            짜장면: 사천원, 짬뽕: 오천원, 탕수육: 만원
-          </DialogContentText>
+          <List className={classes.root}>
+            <ListItem Button>
+              <ListItemText primary="육개장" secondary="2500원" />
+            </ListItem>
+            <ListItem Button>
+              <ListItemText primary="타이어보다 싼 탕수육" secondary="5000원" />
+            </ListItem>
+            <Divider />
+            <ListItem Button onClick={handleOpenSubMenu}>
+              <ListItemText primary="버거류" />
+              {openSubMenu ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={openSubMenu} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItem button>
+                  <ListItemText
+                    primary="와퍼"
+                    secondary="4800원"
+                    className={classes.nested}
+                  />
+                </ListItem>
+                <ListItem button>
+                  <ListItemText
+                    primary="와퍼주니어"
+                    secondary="2900원"
+                    className={classes.nested}
+                  />
+                </ListItem>
+                <ListItem button>
+                  <ListItemText
+                    primary="트리플치즈트러플베이컨와퍼"
+                    secondary="7900원"
+                    className={classes.nested}
+                  />
+                </ListItem>
+              </List>
+            </Collapse>
+            <Divider />
+            <ListItem Button onClick={handleOpenSubMenu2}>
+              <ListItemText primary="음료/사이드" />
+              {openSubMenu2 ? <ExpandLess /> : <ExpandMore />}
+            </ListItem>
+            <Collapse in={openSubMenu2} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItem button>
+                  <ListItemText
+                    primary="콜라M"
+                    secondary="1700원"
+                    className={classes.nested}
+                  />
+                </ListItem>
+                <ListItem button>
+                  <ListItemText
+                    primary="감자튀김"
+                    secondary="1300원"
+                    className={classes.nested}
+                  />
+                </ListItem>
+                <ListItem button>
+                  <ListItemText
+                    primary="소프트콘"
+                    secondary="700원"
+                    className={classes.nested}
+                  />
+                </ListItem>
+              </List>
+            </Collapse>
+          </List>
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCloseMenu} color="primary" autoFocus>
@@ -99,7 +208,7 @@ function OrderMenu() {
       {/* 식당찾기 버튼 */}
       <CustomButton onClick={handleOpenMap}>식당찾기</CustomButton>
       <Dialog
-        open={openEvent}
+        open={openMap}
         onClose={handleCloseMap}
         aria-labelledby="map-title"
         aria-describedby="map-description"
