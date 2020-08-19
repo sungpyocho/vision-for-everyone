876042f2cfe22f0f1d448096db078d0c582b1e5d
sungpyocho <korra0501@gmail.com>
2020-08-19 23:27:19 +0900

    show prop data to receipt

--- client/src/components/Chat/Sections/RecieptMessage.js
+++ client/src/components/Chat/Sections/RecieptMessage.js
@@ -1,7 +1,12 @@
 import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
-import { CardActionArea, CardActions, CardContent, CardMedia } from "@material-ui/core/";
+import {
+  CardActionArea,
+  CardActions,
+  CardContent,
+  CardMedia,
+} from "@material-ui/core/";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import { List, ListItem, ListItemText, Divider } from "@material-ui/core/";
@@ -11,11 +16,11 @@ const useStyles = makeStyles({
     minWidth: 180,
     paddingTop: 10,
     marginBottom: "10px",
-    marginTop: "30px"
+    marginTop: "30px",
   },
 });
 
-function RecieptMessage(props) {
+const RecieptMessage = (props) => {
   const classes = useStyles();
   let endOfMessage = null;
 
@@ -32,36 +37,56 @@ function RecieptMessage(props) {
     >
       <Card className={classes.root}>
         {/* <CardActionArea> */}
-          <CardContent >
-            <Typography gutterBottom variant="h5" component="h2" style={{textAlign:"center"}}>
-              스타벅스 종암DT점에서 주문이 완료되었어요 :)
-            </Typography>
-            <Typography gutterBottom variant="h6" component="h2" style={{textAlign:"center"}}>
-              대기번호
-            </Typography>
-            <Typography gutterBottom variant="h2" component="h2" style={{textAlign:"center"}}>
-              425
-            </Typography>
-            <Typography variant="body1" color="textSecondary" component="p" style={{textAlign:"center"}}>
-              주문하신 메뉴
-            </Typography>
-            <List component="nav">
-              <ListItem>
-                <ListItemText style={{ textAlign: "left"}}>
-                  카페아메리카노(톨)
-                </ListItemText>
-                <ListItemText style={{ textAlign: "right" }}>
-                  4800원
-                </ListItemText>
-              </ListItem>
-              <Divider />
-              <ListItem>
-                <ListItemText style={{ textAlign: "right" }}>
-                  결제하신 금액: 총 4800원
-                </ListItemText>
-              </ListItem>
-            </List>
-          </CardContent>
+        <CardContent>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="h2"
+            style={{ textAlign: "center" }}
+          >
+            {props.orderResult.restaurantName}에서 주문이 완료되었어요 :)
+          </Typography>
+          <Typography
+            gutterBottom
+            variant="h6"
+            component="h2"
+            style={{ textAlign: "center" }}
+          >
+            대기번호
+          </Typography>
+          <Typography
+            gutterBottom
+            variant="h2"
+            component="h2"
+            style={{ textAlign: "center" }}
+          >
+            {Math.floor(Math.random() * 100)}
+          </Typography>
+          <Typography
+            variant="body1"
+            color="textSecondary"
+            component="p"
+            style={{ textAlign: "center" }}
+          >
+            주문하신 메뉴
+          </Typography>
+          <List component="nav">
+            <ListItem>
+              <ListItemText style={{ textAlign: "left" }}>
+                {props.orderResult.menuName}
+              </ListItemText>
+              <ListItemText style={{ textAlign: "right" }}>
+                {props.orderResult.totalAmount}원
+              </ListItemText>
+            </ListItem>
+            <Divider />
+            <ListItem>
+              <ListItemText style={{ textAlign: "right" }}>
+                결제하신 금액: 총 {props.orderResult.totalAmount}원
+              </ListItemText>
+            </ListItem>
+          </List>
+        </CardContent>
         {/* </CardActionArea> */}
       </Card>
       {/* 자동 scroll을 위한 div */}
@@ -73,6 +98,6 @@ function RecieptMessage(props) {
       ></div>
     </div>
   );
-}
+};
 
 export default RecieptMessage;
