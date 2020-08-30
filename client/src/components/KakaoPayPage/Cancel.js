d3ca657fc21802f32896372cc708be72b5204760
Se Joon Kim <sejoondev@gmail.com>
2020-08-31 08:42:11 +0900

    feat: Finish styling cancel, fail pages

--- client/src/components/KakaoPayPage/Cancel.js
+++ client/src/components/KakaoPayPage/Cancel.js
@@ -1,4 +1,17 @@
 import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import cancel from "../../assets/cancel.png";
+
+
+const CancelBlock = styled.div`
+    margin: 0;
+    padding: 0;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    margin-top: 5rem;
+`
 
 function Cancel() {
     window.top.postMessage(JSON.stringify({
@@ -10,9 +23,15 @@ function Cancel() {
     }, [])
     
     return (
-        <div>
-            결제 취소 되었습니다. 다시 주문 부탁드려요.
-        </div>
+        <CancelBlock>
+          <h3>
+            결제 취소 되었습니다.
+          </h3>
+          <div>
+            <img src={cancel} style={{width: '100%'}}/>
+          </div>
+          <h4>다시 주문 부탁드려요.</h4>
+        </CancelBlock>
     )
 }
 
--- client/src/components/KakaoPayPage/Fail.js
+++ client/src/components/KakaoPayPage/Fail.js
@@ -1,4 +1,16 @@
 import React, { useEffect } from 'react'
+import styled from 'styled-components'
+import failCry from "../../assets/fail-cry.png";
+
+const FailBlock = styled.div`
+    margin: 0;
+    padding: 0;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    margin-top: 5rem;
+`
 
 function Fail() {
     window.top.postMessage(JSON.stringify({
@@ -10,9 +22,16 @@ function Fail() {
     }, [])
 
     return (
-        <div>
-            결제 과정에서 오류가 생겼습니다. 다시 주문 부탁드려요.
-        </div>
+        <FailBlock>
+          <h3>
+            결제 과정에서 오류가 생겼습니다.
+          </h3>
+          <div>
+              <img src={failCry} style={{width: '100%'}} />
+          </div>
+          <h4>다시 주문 부탁드려요.</h4>
+        </FailBlock>
+        
     )
 }
 
