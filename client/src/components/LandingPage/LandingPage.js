c5c676387aec8f82b0021b962ef072bd4d4eb716
SnowLeopardMoonBear <43803127+SnowLeopardMoonBear@users.noreply.github.com>
2020-08-31 19:03:17 +0900

    Add new landing page with sample motion and logic

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,32 +1,79 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
-import Top from "./Sections/Top";
-import MiddleOne from "./Sections/MiddleOne";
-import MiddleTwo from "./Sections/MiddleTwo";
-import Bottom from "./Sections/Bottom";
+import { makeStyles } from "@material-ui/core/styles";
+import { Button } from "@material-ui/core";
+import kiwe_motion_1 from "../../assets/kiwe_motion_1.mp4";
+
+const motionArray = [kiwe_motion_1, kiwe_motion_1]; // 모션을 값에 따라 불러오기 위해 저장해두는 배열
 
 const LandingpageBlock = styled.div`
-  height: calc(100% - 64px);
+  height: calc(100% - 56px);
   position: absolute;
-  width: 66.6%;
-  left: 16.7%;
-  right: 16.7%;
-  @media (max-width: 768px) {
-    width: 90%;
-    left: 5%;
-    right: 5%;
-  }
+  width: 100%;
+  background-color: #2fc4b2;
 `;
 
+const MessageBox = styled.div`
+  position: relative;
+  top: 5%;
+  left: 30%;
+  height: 20%;
+  width: 50%;
+  background-color: white;
+  border-radius: 25px;
+  padding: 10px;
+  overflow: hidden;
+  font-size: 30px;
+`;
+
+const MotionBox = styled.video`
+  height: 70%;
+  width: 100%;
+  src: "naver.com";
+`;
+
+const MotionContainer = styled.div`
+  position: relative;
+  height: 50%;
+  top: 10%;
+  left: 10%;
+  height: 70%;
+  width: 80%;
+  background-color: pink;
+`;
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    position: "absolute",
+    top: "90%",
+    right: "5%",
+    height: "40px",
+    borderRadius: "25px",
+    backgroundColor: "pink",
+    padding: "5px",
+  },
+}));
+
 function LandingPage(props) {
+  const classes = useStyles();
+  const [motionNum, setMotionNum] = useState(0);
+  const motionNumHandler = () => {
+    if (motionNum < 10) {
+      setMotionNum(motionNum+1);
+    }
+  };
+
   return (
     <LandingpageBlock>
-      <div style={{ fontSize: "30px", textAlign: "center", fontWeight: "500" }}>
-        <Top props={props} />
-        <MiddleOne />
-        <MiddleTwo />
-        <Bottom />
-      </div>
+      <div
+        style={{ fontSize: "30px", textAlign: "center", fontWeight: "500" }}
+      ></div>
+      <MotionContainer onClick={motionNumHandler}>
+        <MessageBox>안녕하세요, 저는 {motionNum} </MessageBox>
+        <MotionBox src={motionArray[motionNum]} key={motionNum} autoPlay muted>
+        </MotionBox>
+      </MotionContainer>
+      <Button href="/login" className={classes.button}>건너뛸래요</Button>
     </LandingpageBlock>
   );
 }
